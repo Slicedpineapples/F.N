b04de9af475372f961e7600496333a72d51669f8
pc52 <nyabutofelix@outlook.com>
2024-07-30 22:54:50 +0200

    Temporary fixes regarding image upload

--- js/portfolio.js
+++ js/portfolio.js
@@ -51,7 +51,7 @@ function createModalHTML(portfolioItem) {
                                 <div class="modal-body">
                                     <!-- Project details-->
                                     <h2 class="text-uppercase">${portfolioItem.Portfolio_Title}</h2>
-                                    <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                                    <!-- <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p> -->
                                     <img class="img-fluid d-block mx-auto" src="${portfolioItem.Portfolio_Image}" alt="..." />
                                     <p>${portfolioItem.Portfolio_Long_Description}</p>
                                     <ul class="list-inline">
